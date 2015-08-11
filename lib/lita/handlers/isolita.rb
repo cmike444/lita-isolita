31d3686e4c512620aff115a59e6c344504b6aed5
Chris Mikelson <chris.mikelson@640labs.com>
2015-08-11 01:44:38 -0500

    manners functionality

--- lib/lita/handlers/isolita.rb
+++ lib/lita/handlers/isolita.rb
@@ -2,18 +2,20 @@ module Lita
   module Handlers
     class Isolita < Handler
       route(/^(fuck).*/i, :mind_your_manners)
-      route(/^(wake up|get up).*/i, :wake_up)
 
       def mind_your_manners(response)
         reply_to_name = response.user.metadata['mention_name'].nil? ?
                          "#{response.user.name}" :
                         "#{response.user.metadata['mention_name']}"
-        response.reply "Mind your manners #{reply_to_name}.."
+        replies = [
+          "Mind your manners #{reply_to_name}..",
+          "You better wash out that dirty mouth #{reply_to_name}!",
+          "That is just unprofessional, #{reply_to_name}.",
+          "Your mother wouldn't like to hear you say that, #{reply_to_name}."
+        ]
+        response.reply replies.sample
       end
 
-      def wake_up(response)
-        system("heroku ps")
-      end
     end
 
     Lita.register_handler(Isolita)
