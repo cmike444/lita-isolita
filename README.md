dddb3573de093c8e040884b7ec60a35d7312093c
Chris Mikelson <chris.mikelson@640labs.com>
2015-08-11 00:53:55 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# lita-isolita
+
+TODO: Add a description of the plugin.
+
+## Installation
+
+Add lita-isolita to your Lita instance's Gemfile:
+
+``` ruby
+gem "lita-isolita"
+```
+
+## Configuration
+
+TODO: Describe any configuration attributes the plugin exposes.
+
+## Usage
+
+TODO: Describe the plugin's features and how to use them.
---
+++ lib/lita/handlers/isolita.rb
@@ -0,0 +1,21 @@
+module Lita
+  module Handlers
+    class Isolita < Handler
+      route(/^(fuck).*/i, :mind_your_manners)
+      route(/^(wake up|get up).*/i, :wake_up)
+
+      def mind_your_manners(response)
+        reply_to_name = response.user.metadata['mention_name'].nil? ?
+                         "#{response.user.name}" :
+                        "#{response.user.metadata['mention_name']}"
+        response.reply "Mind your manners #{reply_to_name}.."
+      end
+
+      def wake_up(response)
+        system "heroku ps"
+      end
+    end
+
+    Lita.register_handler(Isolita)
+  end
+end
