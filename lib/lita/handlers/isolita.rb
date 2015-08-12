f5eb8408d1e597d2eac5e37a4b63d0f9b0e7e9b1
Chris Mikelson <chris.mikelson@640labs.com>
2015-08-11 20:26:29 -0500

    update manners route; added comments; created reply name

--- lib/lita/handlers/isolita.rb
+++ lib/lita/handlers/isolita.rb
@@ -1,14 +1,24 @@
 module Lita
   module Handlers
     class Isolita < Handler
-      route(/^(fuck).*/i, :mind_your_manners, help: {
-          "Fuck you" => "Advises to clean up your language."
-        })
 
-      def mind_your_manners(response)
+      ## Get the username for who's talking to Lita.
+      def get_reply_to_name(response)
         reply_to_name = response.user.metadata['mention_name'].nil? ?
                          "#{response.user.name}" :
                         "#{response.user.metadata['mention_name']}"
+      end
+
+
+      ## Mind your manners will just focus on "Fuck" 
+      ## for now until others become a problem.
+      route(/fuck/i, :mind_your_manners, help: {
+          "lita: fuck you" => "Advises to clean up your language after saying `fuck`"
+        })
+
+      def mind_your_manners(response)
+        get_reply_to_name(response)
+
         mind_manners = [
           "Mind your manners #{reply_to_name}..",
           "You better wash out that dirty mouth #{reply_to_name}!",
@@ -22,12 +32,14 @@ module Lita
         response.reply mind_manners.sample
       end
 
+
+
       route(/^(show response json).*/i, :print_reply, help: {
-        "show response json" => "Prints out bot response in json"
+        "show response json" => "Prints out Lita's response"
         })
 
       def print_reply(response)
-        response.reply_privately response
+        response.reply_privately "#{response.user}"
       end
 
     end
